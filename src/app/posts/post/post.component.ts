89797734344b5d1db1a3aa9e15c9e510897ea244
Chris.V <cvasqu09@gmail.com>
2018-05-09 22:17:31 -0500

    Started creating the form for the posts

--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+	postForm: FormGroup;
+	editMode: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  	this.postForm = new FormGroup({
+  		'message': new FormControl(null),
+  		'username': new FormControl(null)
+  	});
+  }
+
+  onEdit() {
+  	this.editMode = !this.editMode;
+  }
+
+  onSubmit() {
+  	console.log(this.postForm.controls.message.value);
+  	this.editMode = false;
   }
 
 }
--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostComponent } from './post/post.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
 	declarations: [
 		PostComponent
 	],
 	imports: [
-		CommonModule
+		CommonModule,
+    ReactiveFormsModule
 	],
 	exports:[
 		PostComponent
